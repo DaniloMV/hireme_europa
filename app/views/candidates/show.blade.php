7143a49f921493e858bb440a553569e1de1c9ada
sileence <silence@dessigual.com>
2014-05-15 13:13:19 -0500

    clase 4 - parte 1

---
+++ app/views/candidates/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+
+    <h1>Duilio Palacios</h1>
+
+    <p>
+        Categoría:
+        <a href="">
+            Backend developers
+        </a>
+    </p>
+
+    <p>Tipo de trabajo: Freelance</p>
+
+    <p>
+        Website: <a href="">mejorando.la/php</a>
+    </p>
+
+    <h4>Descripción</h4>
+
+    <p>PHP Senior developer, I love rainbow unicorns</p>
+
+</div> <!-- /container -->
+
+@endsection
